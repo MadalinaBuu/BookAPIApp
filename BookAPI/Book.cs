0378af55abbdf6eaade975e8fd46eddf12216cdd
Madalina Buu <madalina.ciucioiu@gmail.com>
2022-07-13 17:08:35 +0300

    Added Author

--- BookAPI/Book.cs
+++ BookAPI/Book.cs
@@ -7,6 +7,8 @@ namespace BookAPI
         public int Id { get; set; }
         [StringLength(40)]
         public string Title { get; set; } = String.Empty;
+        [StringLength(40)]
+        public string Author { get; set; } = String.Empty;
         public int StatusId { get; set; }
         public Status? Status { get; set; }
         [StringLength(200)]
